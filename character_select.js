26bb6f31f3464597515e98296d7654ceda762e9b
Adeniran Shaddai <shaddaidan@gmail.com>
2023-09-05 05:04:57 +0100

    commit

--- character_select.js
+++ character_select.js
@@ -8,7 +8,16 @@ let char = {
     name : "smith" ,
     specie : "ogun" ,
     rank : 23 ,
+    boost : [1,2,3,4,5], 
     call_out : function (ref){
-        console.log(char.ref)
-    }
+        console.log(char[ref])
+    } ,
+    rank_boost : function(n){
+        let boost = this.rank * this.boost[n]
+        console.log(boost)
+
+    } ,
 }
+
+console.log(char["rank"])
+console.log(char.rank_boost(3))
