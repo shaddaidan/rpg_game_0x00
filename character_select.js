f38d9dff5928989d3784d24b9e9d77bcd6127da8
Adeniran Shaddai <shaddaidan@gmail.com>
2023-09-05 04:45:16 +0100

    shad commit

--- character_select.js
+++ character_select.js
@@ -1,3 +1,14 @@
 // this is a script for the creation and selection of you character
 // i would be using js oop paradimg as well as classes
 // this would also serve as practice for me.
+
+// first we use an object for the character creation.
+
+let char = {
+    name : "smith" ,
+    specie : "ogun" ,
+    rank : 23 ,
+    call_out : function (ref){
+        console.log(char.ref)
+    }
+}
