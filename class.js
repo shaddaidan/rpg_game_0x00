634b6307f3b88dc04e1d6710582df60a2d14c150
Adeniran Shaddai <shaddaidan@gmail.com>
2023-09-06 03:29:49 +0100

    this is a cool commit

---
+++ class.js
@@ -0,0 +1,20 @@
+// this is me going over how to use js classes all over again
+// with the help of chatgpt yeps
+
+// class declaration
+
+class first_class {
+    // class properties and methods would go here
+
+    constructor(power, skill, knowledge, rank, weapons) {
+        this.knowledge = knowledge 
+        this.power = power
+        this.skill = skill
+        this.weapons = weapons
+        this.rank = ranks
+    }
+
+    show_power() {
+        // method logic here
+    }
+}
