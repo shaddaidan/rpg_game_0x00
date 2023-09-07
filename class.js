a76f493726f46fd96881e51233a40d97f2225af1
Adeniran Shaddai <shaddaidan@gmail.com>
2023-09-07 23:03:10 +0100

    this is a commit yeah

--- class.js
+++ class.js
@@ -6,20 +6,35 @@
 class first_class {
     // class properties and methods would go here
 
-    constructor(power, skill, knowledge, rank, weapons) {
+    constructor(name, power, skill, knowledge, rank, weapons) {
+        this.name = name
         this.knowledge = knowledge 
         this.power = power
         this.skill = skill
         this.weapons = weapons
-        this.rank = ranks
+        this.rank = rank
     }
 
     show_power() {
         // method logic here
-        console.log("power is: ", this.power)
+        console.log(this.name, "power is: ", this.power)
     }
 
     show_skill() {
-        console.log("skilled in: " this.skill ) // magic, combat, building, healing
+        console.log(this.name, "is skilled in: ", this.skill ) // magic, combat, building, healing
     }
 }
+
+// creating instances
+
+const barb1 = new first_class('barb1', 'flight', 'healing', 0.4 , 24 , 23 )
+const barb2 = new first_class('barb2', 'firebreathing', 'combat' , 0.2 , 8 , 5)
+
+// Accessing properties and methods
+
+console.log(barb1.power)
+console.log(barb2.power)
+
+barb1.show_power()
+
+barb2.show_skill()
